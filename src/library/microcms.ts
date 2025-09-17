ce810e15a58f9df69e2bb1d799a3c7b3766986ca
syab-syab <syabsyabsyabsyab@gmail.com>
2025-09-17 14:24:40 +0900

    microCMSとの連携テスト成功

---
+++ src/library/microcms.ts
@@ -0,0 +1,33 @@
+// SDK利用準備
+import type { MicroCMSQueries, MicroCMSListContent } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+// 型定義
+// 旧Blog
+export type WebContent = {
+  title: string;
+  content: string;
+} & MicroCMSListContent;
+
+// APIの呼び出し
+// 旧getBlogs
+export const getWebContents = async (queries?: MicroCMSQueries) => {
+  return await client.getList<WebContent>({ endpoint: "webcontents", queries });
+};
+
+// 旧getBlogDetail
+export const getWebContentDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<WebContent>({
+    endpoint: "webcontents",
+    contentId,
+    queries,
+  });
+};
